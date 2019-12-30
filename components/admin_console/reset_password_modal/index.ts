4f58b79a6db2d90da1cad0fbb4b3e3e6d807d25f
ctmusicnz <ctmusicnz@gmail.com>
2019-12-30 17:34:10 +1300

    =removed references to jsx files

--- components/admin_console/reset_password_modal/index.ts
+++ components/admin_console/reset_password_modal/index.ts
@@ -1,21 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {connect} from 'react-redux';
-import {getConfig} from 'mattermost-redux/selectors/entities/general';
-import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
+import { connect } from "react-redux";
+import { getConfig } from "mattermost-redux/selectors/entities/general";
+import { getCurrentUserId } from "mattermost-redux/selectors/entities/users";
 
-import {getPasswordConfig} from 'utils/utils.jsx';
+import { getPasswordConfig } from "utils/utils.jsx";
 
-import ResetPasswordModal from './reset_password_modal.jsx';
+import ResetPasswordModal from "./reset_password_modal";
 
 function mapStateToProps(state) {
-    const config = getConfig(state);
+  const config = getConfig(state);
 
-    return {
-        currentUserId: getCurrentUserId(state),
-        passwordConfig: getPasswordConfig(config),
-    };
+  return {
+    currentUserId: getCurrentUserId(state),
+    passwordConfig: getPasswordConfig(config)
+  };
 }
 
 export default connect(mapStateToProps)(ResetPasswordModal);
